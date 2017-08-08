1e27f99294ee96136e674962252e547e4004afd0
iyerpreethi <30752828+iyerpreethi@users.noreply.github.com>
2017-08-08 20:21:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,84 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles= {
+    articleone: {
+    title:'article-one',
+    date: '8th Aug 2017',
+    content: `
+    <p>
+      This is my first article. This is my first article. This is my first article. This is my first article.
+      This is my first article. This is my first article. This is my first article. This is my first article.
+      This is my first article. This is my first article. This is my first article. This is my first article.
+    </p>
+    <p>
+      This is my first article. This is my first article. This is my first article. This is my first article.
+      This is my first article. This is my first article. This is my first article. This is my first article.
+      This is my first article. This is my first article. This is my first article. This is my first article.
+    </p>
+    <p>
+      This is my first article. This is my first article. This is my first article. This is my first article.
+      This is my first article. This is my first article. This is my first article. This is my first article.
+      This is my first article. This is my first article. This is my first article. This is my first article.
+    </p>`
+    }, 
+    
+    articletwo: {
+    title:'article-two',
+    date: '18th Aug 2017',
+    content: `
+    <p>
+      This is my second article. 
+    </p>`
+    },
+      
+    articlethree: {
+    title:'article-three',
+    date: '28th Aug 2017',
+    content: `
+    <p>
+      This is my third article. 
+    </p>`
+    }
+};
+
+function createtemplate (input) {
+    var title= input.title;
+    var date= input.date;
+    var content= input.content;
+    var htmltemplate= `
+<html>
+  <head>
+      <title>
+          ${title}
+      </title>
+      <meta name="viewport" content= "width= device-width initial scale=1" />
+      <link href="/ui/style.css" rel="stylesheet" />
+  </head>   
+  <body>
+      <div>
+          <a Href='/'>Home</a>
+      </div>
+      <hr/>
+    <div class="container">
+      <div>
+          <h1>
+              Preethi Ganeshan
+          </h1>
+      </div>
+      <div>
+          <h3>
+              ${date}
+          </h3>
+      </div>
+      <div>
+          ${content}
+      </div>
+    </div>
+  </body>
+</html>`;
+return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -18,7 +96,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
