83244bd28f9dc49fe1539aaa6300bde4e2416bd7
iyerpreethi <30752828+iyerpreethi@users.noreply.github.com>
2017-08-10 22:18:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element= document.getElementById("main-text");
 element.innerHTML= ("New Value");
 
-var element= document.getElementById("madi");
-element.click= function () {
-    element.style.marginLeft= '100 px';
+var img= document.getElementById("madi");
+img.click= function () {
+    img.style.marginLeft= '100 px';
 };
