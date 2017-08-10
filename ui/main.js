16be1f8aa50cc609229b7ec21f10814b9080c49c
iyerpreethi <30752828+iyerpreethi@users.noreply.github.com>
2017-08-10 22:54:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ var img= document.getElementById('madi');
 var marginLeft = 0;
     img.onclick = function() {
         setInterval( function() {
+            marginLeft= marginLeft + 10
             img.style.marginLeft = marginLeft+ "10 px";
         } , 100);
     };
