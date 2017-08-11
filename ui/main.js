648658155fc8c9c1bf7f2c00e1fe325d4835fbcd
iyerpreethi <30752828+iyerpreethi@users.noreply.github.com>
2017-08-11 17:11:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ var marginLeft = 0;
     };
     
 var counter = 0;
-var button= document.getElementById ('button');
+var button= document.getElementById ('counter');
 button.onclick = function (){
     counter= counter + 1;
     var span= document.getElementById ('count');
