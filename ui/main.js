baa60f441eaba1319d8537c3aa51ec80480e4ae2
iyerpreethi <30752828+iyerpreethi@users.noreply.github.com>
2017-08-15 22:37:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ var submit= document.getElementById('submit');
 submit.onClick= function(){
     var names=['name1', 'name2', 'name3', 'name4'];
     var list='';
-    for (i=0; i<names.length; i++) {
+    for (var i=0; i<names.length; i++) {
         list += '<li>' +names[i]+ '</li>';
     }
     var ul= document.getElementById('namelist');
